540f85908525fc74d0926bb25ff451dc19e5c4b5
Oleg Saifutdinov <zeusfly@mail.ru>
2022-08-06 17:11:41 +0300

    Created relationship between User & Item models & added storage folder

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
